b3b02000f081a9a29f48a6eaf043c4803f52f4dc
paul <q374536937@gmail.com>
2026-09-11 15:03:53 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>大佬CODE FOR FOOD</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          QUIT IT
         </a>
       </header>
     </div>
